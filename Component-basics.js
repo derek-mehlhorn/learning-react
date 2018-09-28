eca7f80961e25ec7ae647399efb60fc13a901853
Derek Mehlhorn <derekme@microsoft.com>
2018-09-28 12:24:34 -0700

    Re-usable component example leveraging props.

--- Component-basics.js
+++ Component-basics.js
@@ -1,22 +1,47 @@
+// Class Component
 class Button extends React.Component {
-    constructor(props) {
-        super(props);
-      this.state = { count: 0};
-    }
-    
-    handleClick = () => {
-        this.setState({
-          count: this.state.count + 1
-      });
-    };
-    
-    render() {
-        return (
-          <button onClick={this.handleClick} >
-            {this.state.count}
-        </button>
-      );
-    }
-  }
+	handleClick = () => {
+  	this.props.onClickFunc(this.props.amount);
+  };
   
-  ReactDOM.render(<Button label="Do"/>, mountNode);
+  render() {
+  	return (
+    	<button onClick={this.handleClick}>
+      	+{this.props.amount}
+      </button>
+    );
+  }
+}
+
+// Function Component
+const Result = (props) => {
+	return (
+  	<div>{props.label}</div>
+  )
+};
+
+// Class Component which stores shared state that is then
+// used across child components.
+class App extends React.Component {
+  state = { count: 0};
+
+	incCounter = (amount) => {
+  	this.setState((prevState) => ({
+    	count: prevState.count + amount
+    }));
+  };
+
+	render() {
+  	return (
+    	<div>
+        <Button amount={2} onClickFunc={this.incCounter} />
+        <Button amount={5} onClickFunc={this.incCounter} />
+        <Button amount={10} onClickFunc={this.incCounter} />
+        <Result label={this.state.count}/>
+      </div>
+    );
+  }
+}
+
+// React can only mount one element, so everything needs to be hierarchical.
+ReactDOM.render(<App />, mountNode);
