8f2e2c822dc4ecd562a9f24aa0e38532a2ef1f40
Derek Mehlhorn <derekme@microsoft.com>
2018-09-28 12:30:11 -0700

    Update static list of items in response to form submit event.

--- Data-basics.js
+++ Data-basics.js
@@ -10,11 +10,6 @@ const Card = (props) => {
   )
 }
 
-let data = [ 
-    { name: "A", company: "B", avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4" },
-    { name: "A", company: "B", avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4" }
-];
-
 const CardList = (props) => {
 	return (
   	<div>
@@ -23,4 +18,67 @@ const CardList = (props) => {
   );
 }
 
-ReactDOM.render(<CardList cards={data}/>, mountNode);
+class Form extends React.Component {
+	constructor(props) {
+      	super(props);
+        this.state = {username: 'default'};
+    }
+
+	handleChange = (event) => {
+  	    this.setState({username: event.target.value});
+    }
+
+	handleSubmit = (event) => {
+        this.props.callback(); // Invoke callback
+        event.preventDefault(); // mark event as handled.
+    };
+
+	render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" 
+        				value={this.state.username} 
+                placeholder="username" 
+                onChange={this.handleChange} />
+        <button type="submit">Add card</button>
+      </form>
+    );
+  }
+}
+
+class App extends React.Component {
+	
+  constructor(props) {
+  	super(props);
+    
+    this.state = {
+    	data: [ 
+        { name: "Abc", company: "B", avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4" },
+        { name: "Def", company: "B", avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4" }
+		]
+    };
+  }
+  
+  addUser = () => {
+    this.setState((state, props) => {
+      let updated = state.data;
+      updated.push({
+                      name: "zz",
+                      company: "zzz",
+                      avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4"
+                    });
+      return { data: updated };
+    });
+  };
+
+	render() {
+  	return (
+    	<div>
+    		<Form data={this.state.data} callback={this.addUser}/>
+    		<CardList cards={this.state.data} />
+      </div>
+  	);
+  }
+}
+
+ReactDOM.render(<App />, mountNode);
